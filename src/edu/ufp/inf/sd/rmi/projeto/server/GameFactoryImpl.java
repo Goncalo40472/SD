d27451a572ee0831c99146a692bf2ba6d41db555
Gonçalo <goncalo.graca2000@gmail.com>
2023-06-04 16:23:07 +0100

    JWT authentication added

--- src/edu/ufp/inf/sd/rmi/projeto/server/GameFactoryImpl.java
+++ src/edu/ufp/inf/sd/rmi/projeto/server/GameFactoryImpl.java
@@ -3,32 +3,43 @@ package edu.ufp.inf.sd.rmi.projeto.server;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Objects;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.JWTVerifier;
 
 public class GameFactoryImpl extends UnicastRemoteObject implements GameFactoyRI {
 
-    private edu.ufp.inf.sd.rmi.projeto.server.DBMockup db;
+    private final ArrayList<User> users;
     private HashMap<String, GameSessionImpl> sessions;
 
     private HashMap<String, LobbyImpl> lobbies;
 
     private ArrayList<LobbyImpl> lobbiesArray;
 
+    private HashMap<User, String> usersTokens;
+
     public GameFactoryImpl() throws RemoteException {
         super();
-        db = new edu.ufp.inf.sd.rmi.projeto.server.DBMockup();
+        users = new ArrayList();
+        users.add(new User("guest", "ufp"));
         this.lobbies = new HashMap<>();
         this.lobbiesArray = new ArrayList<>();
+        this.usersTokens = new HashMap<>();
     }
 
     @Override
     public boolean register(String username, String pwd) throws RemoteException {
 
-        if(getDB().exists(username, pwd)) return false;
+        if(exists(username, pwd)) return false;
 
-        getDB().register(username, pwd);
-        return getDB().exists(username, pwd);
+        users.add(new User(username, pwd));
+        return exists(username, pwd);
     }
 
     @Override
@@ -36,13 +47,41 @@ public class GameFactoryImpl extends UnicastRemoteObject implements GameFactoyRI
 
         if(sessions == null) sessions = new HashMap<>();
 
-        if(getDB().exists(username, pwd) && !sessions.containsKey(username)){
+        User user = getUser(username, pwd);
 
-            GameSessionImpl session = new GameSessionImpl(this, username, this.lobbies, this.lobbiesArray);
-            sessions.put(username, session);
-            return session;
+        if(exists(username, pwd)){
 
-        } else if (getDB().exists(username, pwd) && sessions.containsKey(username)) {
+            if(!usersTokens.containsKey(user)){
+
+                String token = generateToken(username, pwd, new Date(System.currentTimeMillis() + 1800000));
+                usersTokens.put(user, token);
+                GameSessionImpl session = new GameSessionImpl(this, username, this.lobbies, this.lobbiesArray);
+                sessions.put(username, session);
+                return session;
+            }
+
+            else{
+
+                String token = usersTokens.get(user);
+
+                try {
+                    DecodedJWT decodedJWT = JWT.decode(token);
+                    Algorithm algorithm = Algorithm.HMAC256("secret");
+                    JWTVerifier verifier = JWT.require(algorithm).build();
+                    verifier.verify(decodedJWT);
+                } catch (JWTVerificationException e) {
+                    Date expirationDate = new Date(System.currentTimeMillis() + 1800000);
+                    String newToken = generateToken(username, pwd, expirationDate);
+                    usersTokens.replace(user, token, newToken);
+                }
+
+                GameSessionImpl session = new GameSessionImpl(this, username, this.lobbies, this.lobbiesArray);
+                sessions.put(username, session);
+                return session;
+
+            }
+
+        } else if (exists(username, pwd) && sessions.containsKey(username)) {
 
             return sessions.get(username);
 
@@ -53,12 +92,41 @@ public class GameFactoryImpl extends UnicastRemoteObject implements GameFactoyRI
     }
 
     @Override
-    public DBMockup getDB() {
-        return db;
+    public String generateToken(String username, String pwd, Date expirationDate) throws RemoteException {
+        String token = JWT.create()
+                .withClaim("username", username)
+                .withClaim("password", pwd)
+                .withExpiresAt(expirationDate)
+                .sign(Algorithm.HMAC256("secret"));
+        return token;
     }
 
     @Override
     public void removeSessions(String username) throws RemoteException {
         sessions.remove(username);
+    }
+
+    public boolean exists(String u, String p) {
+        for (User usr : users) {
+            if (Objects.equals(usr.getUname(), u) && Objects.equals(usr.getPword(), p)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public User getUser(String username, String pwd) {
+
+        for (User user : users) {
+
+            if(Objects.equals(user.getUname(), username) && Objects.equals(user.getPword(), pwd)) {
+
+                return user;
+
+            }
+        }
+
+        return null;
+
     }
 }
--- src/edu/ufp/inf/sd/rmi/projeto/server/GameFactoyRI.java
+++ src/edu/ufp/inf/sd/rmi/projeto/server/GameFactoyRI.java
@@ -2,6 +2,7 @@ package edu.ufp.inf.sd.rmi.projeto.server;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Date;
 
 
 public interface GameFactoyRI extends Remote {
@@ -9,7 +10,7 @@ public interface GameFactoyRI extends Remote {
 
     public GameSessionRI login(String username, String pwd) throws RemoteException;
 
-    public DBMockup getDB() throws RemoteException;
+    String generateToken(String username, String password, Date expirationDate) throws RemoteException;
 
     public void removeSessions(String username) throws RemoteException;
 }
